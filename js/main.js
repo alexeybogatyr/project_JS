9b320163596919704eba357b4eb624950b313bbd
alexeybogatyr <bogatyr.alexey@gmail.com>
2020-09-07 10:51:49 +0300

    lesson6

--- js/main.js
+++ js/main.js
@@ -3,33 +3,20 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 const app = new Vue({
     el: '#app',
     data: {
-        catalogUrl: '/catalogData.json',
-        products: [],
-        imgCatalog: 'https://placehold.it/200x150',
-        searchLine: '',
-        isVisibleCart: false
+        userSearch: '',
     },
     methods: {
-        getJson(url) {
+        getJson(url){
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
                     console.log(error);
+                    // активировать компонент ошибки
                 })
         },
-        addProduct(product) {
-            console.log(product.id_product);
-        },
-        FilterGoods(searchLine) {
-            console.log(searchLine);
-        }
     },
-    created() {
-        this.getJson(`${API + this.catalogUrl}`)
-            .then(data => {
-                for (let el of data) {
-                    this.products.push(el);
-                }
-            });
+    mounted() {
+        console.log(this);
     }
 });
+
